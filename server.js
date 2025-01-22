b2418aa7031f0eb9a3fa464efe736e8bb4b97f00
krish21-j <krish.jain.s64@kalvium.community>
2025-01-22 16:11:52 +0530

    api assignment 1

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ Requirements:
 Example Responses:
 - For Monday:
   {
-    "welcomeMessage": "Hello, John! Welcome to our assistant app!",
+    "welcomeMessage": "Hello, xyz! Welcome to our assistant app!",
     "dayMessage": "Happy Monday! Start your week with energy!"
   }
 - For Friday:
@@ -33,7 +33,45 @@ Example Responses:
 Add the required logic below to complete the API.
 */
 
+// console.log(app);
+
+// 1. API - GET /assistant/greet
+// 2. name as query
+// 3. return personalized messages
+
+app.get('/assistant/greet', (request, response) => {
+  let name = request.query.name;
+  let day = new Date().getDay();
+
+  if (day == 1) {
+    let obj = {
+      welcomeMessage: `Hello, ${name}! Welcome to our assistant app!`,
+      dayMessage: 'Happy Monday! Start your week with energy!',
+    };
+    return response.send(obj); //json data stringified
+  } else if (day == 5) {
+    // friday
+    let obj = {
+      welcomeMessage: `Hello, ${name}! Welcome to our assistant app!`,
+      dayMessage: "It's Friday! The weekend is near!",
+    };
+    return response.send(obj); //json data stringified
+  } else {
+    // all other days
+    let obj = {
+      welcomeMessage: `Hello, ${name}! Welcome to our assistant app!`,
+      dayMessage: 'Have a wonderful day!',
+    };
+    return response.send(obj); //json data stringified
+  }
+  response.send('hi');
+});
+
+app.get('/', function (request, response) {
+  return response.send('<p>Hello World</p>'); // html page as normal HTML page
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Virtual Assistant API is running on http://localhost:${PORT}`);
+  console.log(`Virtual Assistant API is running on http://localhost:${PORT}`);
 });
